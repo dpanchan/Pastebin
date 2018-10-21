3e1702eee2b76a7e6bf9ee92e7df15bce6d5953c
dpanchan <dpanchan@kent.edu>
2018-10-21 12:56:51 -0400

    xyz

--- app.py
+++ app.py
@@ -4,33 +4,33 @@ import random as r
 import string
 import os
 
-
-
 app = Flask(__name__)
 
 uploads_dir = os.path.join(os.getcwd(), "uploads")
 length_of_id = 8
 id_chars = string.digits + string.ascii_letters
 
-@app.route("/paste/<paste_id>", methods=['GET'])
+@app.route("/<paste_id>", methods=['GET'])
 def get_paste(paste_id):
     return generate_paste_id()
 
-@app.route("/paste", methods=['POST'])
+@app.route("/upload", methods=['POST'])
 def post_paste():
-    paste_id = store(None)
-    redirect("/paste/{}".format(paste_id))
+    paste_id = store(request.body)
+    return redirect("/paste/{}".format(paste_id))
 
 def store(paste):
     paste_id = generate_paste_id()
-    return paste_id
+    store_path = os.path.join(uploads_dir, paste_id)
+    with open(store_path, "w") as f:
+        f.write(paste)
 
 def generate_paste_id():
     paste_id = None
     while True:
         paste_id = "".join(r.choices(id_chars, k=length_of_id))    
-        file_path = os.path.join(uploads_dir, paste_id)
-        break
+        if not os.path.isfile(uploads_dir, paste_id):
+            break
     return paste_id
 
 app.run(
